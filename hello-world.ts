f6a90ec83a2e02f0bb56ae5e3b25333622efa7c4
Akai165 <147315076+Akai165@users.noreply.github.com>
2024-04-19 15:46:55 +0200

    Update hello-world.ts

--- hello-world.ts
+++ hello-world.ts
@@ -98,33 +98,162 @@ Un sottoalbero è secco se il valore nella radice del sottoalbero è < 0.
 Il taglio avviene eliminando il nodo secco (si veda l'esempio).
 La funzione non deve restituire nulla.
 La soluzione deve essere scritta in TypeScript, 
-definendo opportunamente i tipi, e non usando any o unknown.*/
-
+definendo opportunamente i tipi, e non usando any o unknown.
+*/
 interface TreeNode {
-    sx?: TreeNode;
-    dx?: TreeNode;
-    val: number;
+    sx?: TreeNode | undefined;
+    dx?: TreeNode | undefined;
+    val?: number;
 }
 
-function PotaAlberi(T: TreeNode): void {
+function PotaAlberi(T: TreeNode | undefined): void {
     if(!T) { return }
 
     if(T.val < 0) {
-        T = undefined;
-        return;
+        if (T.val! < 0) {
+            delete T.val;
+            delete T.sx;
+            delete T.dx;
+            return;
+        }
     }
 
     if(T.sx) {
         if(T.sx.val < 0)
-            T.sx = undefined;
+            delete T.sx
         else 
             PotaAlberi(T.sx);
     }
 
     if(T.dx) {
         if(T.dx.val < 0)
-            T.dx = undefined;
+            delete T.dx
         else 
             PotaAlberi(T.dx);
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/*Si scriva in TS una struttura dati generica DrunkenList<T> che implementi 
+una linked list (lista collegata) su degli oggetti generici di tipo Nodo<T>.
+La classe DrunkenList<T> prevede un costruttore senza argomenti per inizializzare 
+una lista vuota, una proprietà length contenente il numero di elementi della lista, 
+ed espone le seguenti operazioni:
+-push che prende come argomento un oggetto di tipo T e lo 
+inserisce in testa se la lista contiene un numero pari di elementi, 
+sia in testa che in coda se contiene un numero dispari.
+-pop che rimuove e ritorna l'elemento in testa alla lista se questa
+ha un numero dispari di elementi, l'elemento in coda se invece la lunghezza è pari. 
+Nel caso la lista sia vuota, la funzione lancia un’eccezione con tipo 
+ReferenceError (non la dovete ridefinire voi).
+as_array che restituisce il contenuto della lista sotto forma di array.
+La soluzione deve essere scritta in TypeScript, tenendo conto della corretta
+dichiarazione dei tipi in ingresso ed in uscita dei metodi (e non usando any o unknown).
+Nota: la lista deve essere implementata in modo collegato (linked).
+*/
+
+class Nodo<T> {
+    value: T;
+    next: Nodo<T> | undefined;
+    prec: Nodo<T> | undefined;
+
+    constructor(value: T){
+        this.value = value;
+        this.next = undefined;
+        this.prec = undefined;
+    }
+}
+
+class DrunkenList<T> {
+    head: Nodo<T> | undefined;
+    tail: Nodo<T> | undefined;
+    lenght: number;
+
+    constructor() {
+        this.lenght = 0;
+        this.head = undefined;
+        this.tail = undefined;
+    }
+
+    insert_tail(value: T) {
+        if(this.tail == undefined) {
+            this.head= new Nodo(value);
+            this.tail = this.head;
+        } else {
+            this.head.prec = new Nodo(value);
+            this.head.prec.next = this.head;
+            this.head = this.head.prec;
+        }
+        this.lenght += 1;
+    }
+
+    insert_head(value: T) {
+        if (this.head == undefined) {
+            this.head = new Nodo(value);
+            this.tail = this.head;
+        } else {
+            this.head.prec = new Nodo(value);
+            this.head.prec.next = this.head;
+            this.head = this.head.prec;
+        }
+        this.length += 1;
+    }
+
+    remove_tail(): T {
+        if(this.tail === undefined) {
+            throw new ReferenceError();
+        }
+
+        let val = this.tail.value;
+
+        if(this.tail.prec === undefined) {
+            this.head = undefined;
+            this.tail = undefined;
+        } else {
+            this.tail.prec.next = undefined;
+            this.tail = this.tail.prec;
+        }
+
+        this.lenght -= 1;
+        return val;
+    }
+
+    remove_head() {
+        if(this.head === undefined) {
+            throw new ReferenceError();
+        }
+
+        let val = this.head.value;
+
+        if(this.head.next === undefined) {
+            this.head = undefined;
+            this.tail = undefined;
+        } else {
+            this.head.next.prec = undefined;
+            this.head = this.head.next;
+        }
+
+        this.lenght -= 1;
+        return val;
+    }
+
+    push(value: T) {
+        this.insert_head(value);
+
+        if((this.lenght-1) % 2 == 0) {
+            return this.remove_tail();
+        } else {
+            return this.remove_head()
+        }
+    }
+
+    as_array(): T[] {
+        let ptr = this.head;
+        let arr = [];
+        while (ptr != undefined) {
+            arr.push(ptr.value);
+            ptr = ptr.next;
+        }
+        return arr;
     }
 }
